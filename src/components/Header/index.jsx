bf8380501e26ecf23ba08c39256cf5d2a563446c
heiheiyaya <huwanbinya@163.com>
2022-01-05 21:21:53 +0800

    完善首页和注销功能

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,98 @@
 import React, { Component } from 'react'
+import { Modal } from 'antd'
+import { Navigate } from 'react-router-dom'
+
+import { formateDate } from '../../utils/dateUtils'
+import menuList from '../../config/menuConfig'
+import memoryUtils from '../../utils/memoryUtils'
+import storageUtils from '../../utils/storageUtils'
 import './index.less'
 
 
+
+
 export default class Header extends Component {
+    state = {
+        currentTime: formateDate(Date.now()),
+
+
+    }
+    logout = () => {
+        // 显示确认提示
+        Modal.confirm({
+            title: '确认退出吗?',
+            okText: '确认',
+            cancelText: '取消',
+            onOk: () => {
+
+                // 确定后, 删除存储的用户信息
+                // local中的
+                storageUtils.removeUser()
+                // 内存中的
+                memoryUtils.user = {}
+                // 跳转到登陆界面
+            },
+            onCancel() {
+
+            },
+        })
+
+    }
+    getTitle = () => {
+        let title = ''
+        const path = window.location.pathname
+        menuList.forEach(item => {
+            if (item.key === path) {
+                title = item.title
+            } else if (item.children) {
+                const cItem = item.children.find(cItem => path.indexOf(cItem.key) === 0)
+                if (cItem) {
+                    title = cItem.title
+                }
+            }
+
+        })
+
+
+        return title
+    }
+
+    UNSAFE_componentDidMount() {
+        // 启动循环定时器
+        this.intervalId = setInterval(() => {
+            // 将currentTime更新为当前时间值
+            this.setState({
+                currentTime: formateDate(Date.now())
+            })
+        }, 1000);
+
+    }
+    UNSAFE_componentWillUnmount() {
+        // 清除定时器
+        clearInterval(this.intervalId)
+    }
     render() {
+        const user = memoryUtils.user
+        // 得到当前需要显示的title
+        const title = this.getTitle()
+        // 退出后自动跳转到指定的登录页面
+        const users = memoryUtils.user
+        if (Object.keys(users).length === 0) {
+            return <Navigate to='/' replace='true' />
+        }
+
         return (
             <div className="header">
                 <div className="header-top">
-
+                    欢迎, {user.data.username} &nbsp;&nbsp;
+                    <a onClick={this.logout}>退出</a>
                 </div>
                 <div className="header-bottom">
-                    <div className="header-bottom-left"></div>
+                    <div className="header-bottom-left">{title}</div>
                     <div className="header-bottom-right">
+                        <span>{this.state.currentTime}</span>
+                        <img src="" alt="weather" />
+                        <span>晴</span>
                     </div>
                 </div>
             </div>
--- src/config/menuConfig.js
+++ src/config/menuConfig.js
@@ -1,23 +1,23 @@
 const menuList = [
   {
     title: '首页', // 菜单标题名称
-    key: '/admin/home', // 对应的path
+    key: '/Admin/home', // 对应的path
     icon: 'home', // 图标名称
     public: true, // 公开的
   },
   {
     title: '商品',
-    key: '/admin/products',
+    key: '/Admin/products',
     icon: 'appstore',
     children: [ // 子菜单列表
       {
         title: '品类管理',
-        key: '/admin/category',
+        key: '/Admin/category',
         icon: 'bars'
       },
       {
         title: '商品管理',
-        key: '/admin/product',
+        key: '/Admin/product',
         icon: 'tool'
       },
     ]
@@ -25,33 +25,33 @@ const menuList = [
 
   {
     title: '用户管理',
-    key: '/admin/user',
+    key: '/Admin/user',
     icon: 'user'
   },
   {
     title: '角色管理',
-    key: '/admin/role',
+    key: '/Admin/role',
     icon: 'safety',
   },
 
   {
     title: '图形图表',
-    key: '/admin/chart',
+    key: '/Admin/chart',
     icon: 'area-chart',
     children: [
       {
         title: '柱形图',
-        key: '/admin/charts/bar',
+        key: '/Admin/charts/bar',
         icon: 'bar-chart'
       },
       {
         title: '折线图',
-        key: '/admin/charts/line',
+        key: '/Admin/charts/line',
         icon: 'line-chart'
       },
       {
         title: '饼图',
-        key: '/admin/charts/pie',
+        key: '/Admin/charts/pie',
         icon: 'pie-chart'
       },
     ]
